a2cac437b46eb4987b9a10a4d36fe003262c45ec
Shubham Singh <shubhamsingh7167424@gmail.com>
2025-01-05 18:26:58 +0530

    first program

---
+++ Main.java
@@ -0,0 +1,9 @@
+public  class  Main{
+    
+    public static void main(String[] args) {
+        
+        System.out.println("i like pizza");
+        System.out.println("i am lost");
+        System.out.println("i am a boring person");
+    }
+}   
