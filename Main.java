f22993aae343c5f056685f0e2c04f032f3ebd7a6
Shubham Singh <shubhamsingh7167424@gmail.com>
2025-01-05 21:10:50 +0530

    data types in java

--- Main.java
+++ Main.java
@@ -1,9 +1,38 @@
+
+
 public  class  Main{
     
     public static void main(String[] args) {
         
-        System.out.println("i like pizza");
-        System.out.println("i am lost");
-        System.out.println("i am a boring person");
+        int age=55;
+        int year=2025;
+        int quantity = 1;
+
+        double price = 18999;
+        double gpa = 3.5;
+        double temperature = -13.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = false;
+        boolean forSale = true;
+        boolean isOnline = true;
+
+        String name = "Bro Code";
+        String food = "Pizza";
+        String email = "fake123@gmail.com";
+        String car = "Mustang";
+        String color = "Red";
+
+        System.out.println("Your choice is a " +color+ " " +year+ " " +car);
+        System.out.println("The price is " +currency +price);
+
+        if(forSale){
+            System.out.println("There is a " + car + " for sale.");            
+        }else{
+            System.out.println("The " +car + " is not for sale.");
+        }
     }
 }   
