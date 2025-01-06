e04571d037895de7181c08ad26851815d6224871
Shubham Singh <shubhamsingh7167424@gmail.com>
2025-01-06 17:02:00 +0530

    refactor input handling in  to improve user experience

--- Primitive_data_Type.java
+++ Primitive_data_Type.java
@@ -6,27 +6,28 @@ public  class  Primitive_data_Type{
       public static void main(String[] args){
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Enter your name: ");
-            String name = scanner.nextLine();
+            // Common Issue
+
+            // nextInt() only eats numbers
+
+            // nextLine() eats everything until it finds an Enter key [1]
+
+            // When you use nextInt(), it leaves the Enter key behind
+
+            // Adding an extra nextLine() eats up that leftover Enter key [2]
+
+            // Now your next nextLine() starts fresh! [3]
 
             System.out.print("Enter your age: ");
             int age = scanner.nextInt();
+            scanner.nextLine();
 
-            System.out.print("Enter your gpa: ");
-            double gpa = scanner.nextDouble();
+            System.out.print("Enter your favourite Color: ");
+            String color = scanner.nextLine();
 
-            System.out.print("Are you a student? (true/false): ");
-            boolean isStudent = scanner.nextBoolean();
+            System.out.println("You are " +age + " years old");
+            System.out.println("You like the color " +color);
 
-            System.out.println("Hello " +name);
-            System.out.println("You are " + age + " years old");
-            System.out.println("Your gpa is " +gpa);
-
-            if(isStudent){
-                  System.out.println("You are enrolled as a student");
-            }else{
-                  System.out.println("You are not enrolled");
-            }
 
             scanner.close();
       }
