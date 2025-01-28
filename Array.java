f575dbf24fb4711d6ce244b07e7fddaa5b8b5193
Shubham Singh <shubhamsingh7167424@gmail.com>
2025-01-28 16:20:46 +0530

    add Array class to demonstrate usage of Arrays.fill method

---
+++ Array.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Array{
+      public static void main(String[] args) {
+          String[] fruits = {"apple", "orange", "banana", "coconut"};
+
+          // fruits[0] = "pineapple";
+          // int numOfFruits = fruits.length;
+          // Arrays.sort(fruits);
+          Arrays.fill(fruits, "pineapple");
+
+            for(String fruit : fruits){
+                  System.out.println(fruit);
+            }
+      }
+}
